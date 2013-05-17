2a7bc1d3bbf7bb014be4604bd6dffff0750accb9
Nicholas Spencer <nick.switch@gmail.com>
2013-05-17 18:11:08 -0500

    Initial Commit

---
+++ AllTheSingleLadies/MAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAAppDelegate.h
+//  AllTheSingleLadies
+//
+//  Created by Nicholas Spencer on 5/17/13.
+//  Copyright (c) 2013 Nicholas Spencer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AllTheSingleLadies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AllTheSingleLadies
+//
+//  Created by Nicholas Spencer on 5/17/13.
+//  Copyright (c) 2013 Nicholas Spencer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAAppDelegate class]));
+    }
+}
